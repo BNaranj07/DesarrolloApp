fe43636e284e49188216469c59f5da2846a1dd20
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-02 03:10:51 +0000

    Semana 3 Practica en vista 2

--- lib/pages/page2/page2_model.dart
+++ lib/pages/page2/page2_model.dart
@@ -1,11 +1,33 @@
+import '/flutter_flow/flutter_flow_data_table.dart';
+import '/flutter_flow/flutter_flow_timer.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'page2_widget.dart' show Page2Widget;
 import 'package:flutter/material.dart';
 
 class Page2Model extends FlutterFlowModel<Page2Widget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for PaginatedDataTable widget.
+  final paginatedDataTableController =
+      FlutterFlowDataTableController<dynamic>();
+  // State field(s) for Timer widget.
+  final timerInitialTimeMs = 60000;
+  int timerMilliseconds = 60000;
+  String timerValue = StopWatchTimer.getDisplayTime(
+    60000,
+    hours: false,
+    milliSecond: false,
+  );
+  FlutterFlowTimerController timerController =
+      FlutterFlowTimerController(StopWatchTimer(mode: StopWatchMode.countDown));
+
   @override
   void initState(BuildContext context) {}
 
   @override
-  void dispose() {}
+  void dispose() {
+    paginatedDataTableController.dispose();
+    timerController.dispose();
+  }
 }
--- lib/pages/page2/page2_widget.dart
+++ lib/pages/page2/page2_widget.dart
@@ -1,5 +1,8 @@
+import '/flutter_flow/flutter_flow_data_table.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_timer.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:flutter/material.dart';
 import 'page2_model.dart';
 export 'page2_model.dart';
@@ -21,6 +24,8 @@ class _Page2WidgetState extends State<Page2Widget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => Page2Model());
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
   }
 
   @override
@@ -53,11 +58,183 @@ class _Page2WidgetState extends State<Page2Widget> {
           centerTitle: false,
           elevation: 2.0,
         ),
-        body: const SafeArea(
+        body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [],
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(0.0, 0.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.network(
+                              'https://static.independentespanol.com/2022/09/30/14/SK2022%2010%20smr-1.jpg',
+                              width: 200.0,
+                              height: 200.0,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: FlutterFlowDataTable<dynamic>(
+                      controller: _model.paginatedDataTableController,
+                      data: paginatedDataTableRecordList,
+                      columnsBuilder: (onSortChanged) => [
+                        DataColumn2(
+                          label: DefaultTextStyle.merge(
+                            softWrap: true,
+                            child: Text(
+                              'Primera',
+                              style: FlutterFlowTheme.of(context)
+                                  .labelLarge
+                                  .override(
+                                    fontFamily: 'Inter',
+                                    color: const Color(0xFFF0F3F6),
+                                    letterSpacing: 0.0,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                            ),
+                          ),
+                        ),
+                        DataColumn2(
+                          label: DefaultTextStyle.merge(
+                            softWrap: true,
+                            child: Text(
+                              'Segunda',
+                              style: FlutterFlowTheme.of(context)
+                                  .labelLarge
+                                  .override(
+                                    fontFamily: 'Inter',
+                                    color: const Color(0xFFF9F9F9),
+                                    letterSpacing: 0.0,
+                                  ),
+                            ),
+                          ),
+                        ),
+                        DataColumn2(
+                          label: DefaultTextStyle.merge(
+                            softWrap: true,
+                            child: Text(
+                              'Tercera',
+                              style: FlutterFlowTheme.of(context)
+                                  .labelLarge
+                                  .override(
+                                    fontFamily: 'Inter',
+                                    color: const Color(0xFFF6F8F9),
+                                    letterSpacing: 0.0,
+                                  ),
+                            ),
+                          ),
+                        ),
+                      ],
+                      dataRowBuilder: (Item, paginatedDataTableIndex, selected,
+                              onSelectChanged) =>
+                          DataRow(
+                        color: WidgetStateProperty.all(
+                          paginatedDataTableIndex % 2 == 0
+                              ? FlutterFlowTheme.of(context).secondaryBackground
+                              : FlutterFlowTheme.of(context).primaryBackground,
+                        ),
+                        cells: [
+                          Text(
+                            'Edit Column 1',
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                          ),
+                          Text(
+                            'Edit Column 2',
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                          ),
+                          Text(
+                            'Edit Column 3',
+                            style: FlutterFlowTheme.of(context)
+                                .bodyMedium
+                                .override(
+                                  fontFamily: 'Inter',
+                                  letterSpacing: 0.0,
+                                ),
+                          ),
+                        ].map((c) => DataCell(c)).toList(),
+                      ),
+                      paginated: true,
+                      selectable: false,
+                      hidePaginator: false,
+                      showFirstLastButtons: false,
+                      headingRowHeight: 56.0,
+                      dataRowHeight: 48.0,
+                      columnSpacing: 20.0,
+                      headingRowColor: const Color(0xFFF9060A),
+                      borderRadius: BorderRadius.circular(8.0),
+                      addHorizontalDivider: true,
+                      addTopAndBottomDivider: false,
+                      hideDefaultHorizontalDivider: true,
+                      horizontalDividerColor:
+                          FlutterFlowTheme.of(context).secondaryBackground,
+                      horizontalDividerThickness: 1.0,
+                      addVerticalDivider: false,
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0.0, 0.57),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    FlutterFlowTimer(
+                      initialTime: _model.timerInitialTimeMs,
+                      getDisplayTime: (value) => StopWatchTimer.getDisplayTime(
+                        value,
+                        hours: false,
+                        milliSecond: false,
+                      ),
+                      controller: _model.timerController,
+                      updateStateInterval: const Duration(milliseconds: 1000),
+                      onChanged: (value, displayTime, shouldUpdate) {
+                        _model.timerMilliseconds = value;
+                        _model.timerValue = displayTime;
+                        if (shouldUpdate) safeSetState(() {});
+                      },
+                      textAlign: TextAlign.start,
+                      style:
+                          FlutterFlowTheme.of(context).headlineSmall.override(
+                                fontFamily: 'Inter Tight',
+                                fontSize: 70.0,
+                                letterSpacing: 0.0,
+                              ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
